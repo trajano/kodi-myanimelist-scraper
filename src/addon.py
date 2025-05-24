36176f72954c0bd644c980b92e88062428d6b996
Archimedes Trajano <archimedes.trajano@iblum.co>
2025-05-24 19:46:52 -0400

    x

--- src/addon.py
+++ src/addon.py
@@ -2,18 +2,30 @@ import sys
 import urllib.parse
 
 import xbmcplugin
-from metadata_myanimelist_tv import route
+# from metadata_myanimelist_tv import route
 
 
 def get_params() -> dict[str, str]:
+    # def get_params():
     param_string = sys.argv[2][1:]
     if param_string:
         return dict(urllib.parse.parse_qsl(param_string))
     return {}
 
 
+print(__name__)
 params = get_params()
 plugin_handle = int(sys.argv[1])
 action = params.get("action")
-route(params)
+
+
+print(params)
+print(params)
+print(params)
+print(params)
+print(params)
+print(params)
+print(params)
+print(params)
+# route(params)
 xbmcplugin.endOfDirectory(plugin_handle)
