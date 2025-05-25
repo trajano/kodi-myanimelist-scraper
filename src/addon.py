62ac9fa4596e2c7ba24443f87cc517122b028708
Archimedes Trajano <developer@trajano.net>
2025-05-24 23:43:29 -0400

    X

--- src/addon.py
+++ src/addon.py
@@ -1,9 +1,8 @@
 import sys
 import urllib.parse
-import xbmc
 import xbmcplugin
 
-# from metadata_myanimelist_tv import route
+from metadata_myanimelist_tv import route
 from typing import Dict
 
 
@@ -15,15 +14,13 @@ def get_params() -> Dict[str, str]:
     return {}
 
 
-params = get_params()
-plugin_handle = int(sys.argv[1])
-action = params.get("action")
+if __name__ == "__main__":
+    params = get_params()
+    plugin_handle = int(sys.argv[1])
+    action = params.get("action")
 
-# __name__ == __main__
-# args ['plugin://metadata.myanimelist.tv/', '575', '?action=find&pathSettings=%7b%7d&title=Foo%203', 'resume:false']
-# params {'action': 'find', 'pathSettings': '{}', 'title': 'Foo 3'}
-xbmc.log(f"name {__name__}", xbmc.LOGWARNING)
-xbmc.log(f"args {sys.argv}", xbmc.LOGWARNING)
-xbmc.log(f"params {params}", xbmc.LOGWARNING)
-# route(params)
-xbmcplugin.endOfDirectory(plugin_handle)
+    # __name__ == __main__
+    # args ['plugin://metadata.myanimelist.tv/', '575', '?action=find&pathSettings=%7b%7d&title=Foo%203', 'resume:false']
+    # params {'action': 'find', 'pathSettings': '{}', 'title': 'Foo 3'}
+    route(params)
+    xbmcplugin.endOfDirectory(plugin_handle)
--- src/metadata_myanimelist_tv/router.py
+++ src/metadata_myanimelist_tv/router.py
@@ -1,2 +1,16 @@
-def route(params: dict[str, str]):
-    print("Hello from myanimelist-scraper!")
+from typing import Dict
+import xbmc
+
+
+def find(*, title: str, pathSettings: str):
+    pass
+
+
+def route(params: Dict[str, str]):
+    action = params.get("action")
+    if action is None:
+        raise ValueError("action is not present in the parameters")
+    elif action == "find":
+        find(**params)
+    else:
+        xbmc.log(f"unsupported action: {action}", xbmc.LOGWARNING)
