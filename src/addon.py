bbdd3d5fc1ce6d2fde2208967888fa116990d412
Archimedes Trajano <developer@trajano.net>
2025-05-24 23:14:36 -0400

    re-added types

--- src/addon.py
+++ src/addon.py
@@ -3,41 +3,25 @@ import urllib.parse
 import xbmc
 import xbmcplugin
 # from metadata_myanimelist_tv import route
+from typing import List, Dict, Optional
 
-
-#def get_params() -> dict[str, str]:
-def get_params():
+def get_params() -> Dict[str, str]:
+# def get_params():
     param_string = sys.argv[2][1:]
     if param_string:
         return dict(urllib.parse.parse_qsl(param_string))
     return {}
 
 
-print(__name__)
 params = get_params()
 plugin_handle = int(sys.argv[1])
 action = params.get("action")
 
-
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-print(params)
-xbmc.log(f"args {sys.argv}")
-xbmc.log(f"params {params}")
+# __name__ == __main__
+# args ['plugin://metadata.myanimelist.tv/', '575', '?action=find&pathSettings=%7b%7d&title=Foo%203', 'resume:false']
+# params {'action': 'find', 'pathSettings': '{}', 'title': 'Foo 3'}
+xbmc.log(f"name {__name__}", xbmc.LOGWARNING)
+xbmc.log(f"args {sys.argv}", xbmc.LOGWARNING)
+xbmc.log(f"params {params}", xbmc.LOGWARNING)
 # route(params)
 xbmcplugin.endOfDirectory(plugin_handle)
