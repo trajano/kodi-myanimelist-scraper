0b2310c10832a71a9aaa77408f00d0e6093bf515
Archimedes Trajano <developer@trajano.net>
2025-05-26 00:36:37 -0400

    X

---
+++ src/metadata_myanimelist_tv/_settings.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from dataclasses_json import (
+    dataclass_json,
+    Undefined,
+    DataClassJsonMixin,
+)
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class AddOnSettings(DataClassJsonMixin):
+    client_id: str
